9eb83c53fe487c7e1161a34f7c54ec062b509427
Shiva <shiva@pop-os.GuestWifi>
2024-11-27 11:40:11 +0100

    mv_data.sh completed

--- mv_data.sh
+++ mv_data.sh
@@ -42,17 +42,6 @@ for FILE in $FILES; do
 done
 echo "Pretrained models copied to $CONTAINER_ID:/DAFormer-SCDD/pretrained"
 
-## Navigate to work_dirs download and extract files
-#docker exec -it "$CONTAINER_ID" bash -c "
-#    cd /DAFormer-SCDD/work_dirs && \
-#    echo 'Downloading file from Google Drive...' && \
-#    wget --no-check-certificate https://drive.google.com/uc?export=download&id=1pG3kDClZDGwp1vSTEXmTchkGHmnLQNdP
-# -O downloaded_file.zip || \
-#    curl -L https://drive.google.com/uc?export=download&id=1pG3kDClZDGwp1vSTEXmTchkGHmnLQNdP
-# -o downloaded_file.zip
-#    "
-#
-
 # WORK_DIRS
 # Copy tar.gz fiel to work_dirs directory downloaded from https://drive.google.com/file/d/1pG3kDClZDGwp1vSTEXmTchkGHmnLQNdP/view
 if ! test -f /home/shiva/Desktop/211108_1622_gta2cs_daformer_s0_7f24c.tar.gz; then
@@ -65,17 +54,63 @@ if docker exec "$CONTAINER_ID" test -f "/DAFormer-SCDD/work_dirs/211108_1622_gta
     echo "File 211108_1622_gta2cs_daformer_s0_7f24c.tar.gz already exists inside the container. Skipping..."
 else
     docker cp /home/shiva/Desktop/211108_1622_gta2cs_daformer_s0_7f24c.tar.gz "$CONTAINER_ID:/DAFormer-SCDD/work_dirs"
-fi
-
-echo "The .tar.gz file copied into the work_dirs directory"
-
-# unzip and remove the zip file
-docker exec -it "$CONTAINER_ID" bash -c "
+    # unzip and remove the zip file
+    docker exec -it "$CONTAINER_ID" bash -c "
     cd /DAFormer-SCDD/work_dirs && \
     tar -xzvf 211108_1622_gta2cs_daformer_s0_7f24c.tar.gz &&\
     rm 211108_1622_gta2cs_daformer_s0_7f24c.tar.gz
     "
-echo "work_dirs files unzipped and original file deleted"
+    echo "work_dirs files unzipped and original file deleted"
+fi
+
+echo "The .tar.gz file copied into the work_dirs directory"
 
 # CITYSCAPES DATASET
+if [[ ! -f /home/shiva/Desktop/leftImg8bit_trainvaltest.zip && ! -f /home/shiva/Desktop/gtFine_trainvaltest.zip ]]; then 
+    echo "Cityscapes dataset is not downloaded. Exiting..."
+    exit 1
+fi
+# Check if the file already exists inside the container
+if docker exec "$CONTAINER_ID" test -d "/DAFormer-SCDD/data/cityscapes/gtFine"; then
+    echo "cityscapes data exist, skipping..."
+else
+    docker exec -it "$CONTAINER_ID" bash -c "mkdir -p data/cityscapes"
+    docker cp /home/shiva/Desktop/leftImg8bit_trainvaltest.zip "$CONTAINER_ID:/DAFormer-SCDD/data/cityscapes"
+    docker cp /home/shiva/Desktop/gtFine_trainvaltest.zip "$CONTAINER_ID:/DAFormer-SCDD/data/cityscapes"  
+    # unzip and remove the zip files
+    docker exec -it "$CONTAINER_ID" bash -c "
+    cd /DAFormer-SCDD/data/cityscapes && \
+    unzip leftImg8bit_trainvaltest.zip && \
+    unzip gtFine_trainvaltest.zip && \
+    rm leftImg8bit_trainvaltest.zip gtFine_trainvaltest.zip
+    "
+    echo "cityscapes dataset copied, unzipped, and original file deleted"
+fi
+
+docker exec -it "$CONTAINER_ID" bash -c "python tools/convert_datasets/cityscapes.py data/cityscapes --nproc 8"
+
 # GTA DATASET
+if [[ ! -f /home/shiva/Desktop/10_images.zip && ! -f /home/shiva/Desktop/10_labels.zip ]]; then 
+    echo "gta dataset is not downloaded. Exiting..."
+    exit 1
+fi
+# Check if the file already exists inside the container
+if docker exec "$CONTAINER_ID" test -d "/DAFormer-SCDD/data/gta/images"; then
+    echo "gta data exist, skipping..."
+else
+    docker exec -it "$CONTAINER_ID" bash -c "mkdir -p /DAFormer-SCDD/data/gta"
+    for f in /home/shiva/Desktop/*_images.zip; do docker cp "$f" "$CONTAINER_ID:/DAFormer-SCDD/data/gta"; done
+    echo "gta image zip files copied"
+    for f in /home/shiva/Desktop/*_labels.zip; do docker cp "$f" "$CONTAINER_ID:/DAFormer-SCDD/data/gta"; done
+    echo " gta label zip files copied"
+    # unzip and remove the zip files
+    docker exec -it "$CONTAINER_ID" bash -c "
+    cd /DAFormer-SCDD/data/gta && \
+    unzip '*.zip' && \
+     for f in /home/shiva/Desktop/*_images.zip; do docker cp $f "$CONTAINER_ID:/DAFormer-SCDD/data/cityscapes"; done
+   rm *.zip
+    "
+    echo "gta dataset copied, unzipped, and original file deleted"
+fi
+
+docker exec -it "$CONTAINER_ID" bash -c "python tools/convert_datasets/gta.py data/gta --nproc 8"
